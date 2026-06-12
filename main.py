e6b1c2e98a22f45579f08625db1bff30801c8ad8
samsonmorara-maker/samsongit config  --global user.email samson.morara@student.moringaschool.com <samson.morara@student.moringaschool.com>
2026-06-12 13:30:25 +0300

    error

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from task_utils import (
     add_task,
     mark_task_as_complete,
     view_pending_tasks,
-    calculate_progress,
-    tasks
+    calculate_progress
 )
-
 def main():
     while True:
         print("\nTask Management System")
